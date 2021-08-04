d52997e87821a0cdf9fa48a87d05a958dea9ca72
Arsen339 <arsensubbotin@yandex.ru>
2021-08-04 15:48:49 +0500

    Exception processing added

--- class_multi_threading.py
+++ class_multi_threading.py
@@ -11,21 +11,27 @@ class Fisher(Thread):
         self.worms = worms
 
     def run(self):
-        catch = defaultdict(int)
-        for worm in range(self.worms):
-            print(f"{self.name}: Червяк номер {worm} запущен, ждем...\n", flush=True)
-            _ = 3 ** (random.randint(50, 70) * 10000)  # Задержка
-            fish = random.choice(FISH)
-            if fish is None:
-                print(f"{self.name}: Тьфу ты, сожрали червя\n", flush=True)
-            else:
-                print(f"{self.name}:Ага, у меня {fish}\n", flush=True)
-                catch[fish] += 1
+        try:
+            #  Обработка исключений в потоке
+            catch = defaultdict(int)
+            for worm in range(self.worms):
+                print(f"{self.name}: Червяк номер {worm} запущен, ждем...\n", flush=True)
+                _ = 3 ** (random.randint(50, 70) * 10000)  # Задержка
+                fish = random.choice(FISH)
+                dice = random.randint(1, 5)
+                if self.name == "Коля" and dice == 1:
+                    raise ValueError(f"Блин, у меня сломалась удочка на {worm} черве")
+                if fish is None:
+                    print(f"{self.name}: Тьфу ты, сожрали червя\n", flush=True)
+                else:
+                    print(f"{self.name}:Ага, у меня {fish}\n", flush=True)
+                    catch[fish] += 1
 
-        print(f"Итого рыбак {self.name} поймал:\n")
-        for fish, count in catch.items():
-            print(f"{fish} - {count}")
-
+            print(f"Итого рыбак {self.name} поймал:\n")
+            for fish, count in catch.items():
+                print(f"{fish} - {count}")
+        except Exception as exc:
+            print(exc)
 
 
 vasya = Fisher(name='Вася', worms=10)
@@ -36,7 +42,13 @@ print('Они пошли на рыбалку ...................................
 vasya.start()
 kolya.start()
 
+
+
 print("Ждем, пока они вернутся......................................\n")
+
+
 vasya.join()
 kolya.join()
+
+
 print("Итак, они вернулись .........................................\n")
